1f8724b3ea9f97b0f83db2af2857b265a77cfbf5
nitesh098suhar <nitesh098suthar@gmail.com>
2024-09-03 22:37:41 +0530

    header footer and toggle mode added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+import Footer from "@/components/ui/footer";
+import Header from "@/components/ui/header";
+import Image from "next/image";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="relative top-0 left-0">
+      <div className=" overflow-hidden">
+        <div className="relative">
+          <Image
+            width={1000}
+            height={1000}
+            src="/images/main-image.jpg"
+            alt="main-image"
+            className="w-full h-full"
+          />
+        </div>
+        <div className="bg-black h-full w-full absolute top-0 bottom-0 opacity-70 "></div>
+      </div>
+      <div className="absolute top-0 left-0 w-full">
+        <Header />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ components/ui/header.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import React from "react";
+import { ModeToggle } from "./mode-toggle";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center w-full px-8">
+      <Image
+        width={60}
+        height={60}
+        src="/images/logo.jpg"
+        alt="logo"
+        className=""
+      />
+      <div className="flex gap-4">
+        <p className="text-background/60 hover:text-background/80 cursor-pointer">
+          Home
+        </p>
+        <p className="text-background/60 hover:text-background/80 cursor-pointer">
+          All Tops
+        </p>
+        <p className="text-background/60 hover:text-background/80 cursor-pointer">
+          All Bottoms
+        </p>
+        <p className="text-background/60 hover:text-background/80 cursor-pointer">
+          New Arrivals
+        </p>
+        <p className="text-background/60 hover:text-background/80 cursor-pointer">
+          Most Sold
+        </p>
+        <p className="text-background/60 hover:text-background/80 cursor-pointer">
+          Wholesale
+        </p>
+      </div>
+      <div>
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
