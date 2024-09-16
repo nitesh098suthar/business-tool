45d26e97c15b53242ff44a44b84812c8ef3bd5bb
nitesh098suhar <nitesh098suthar@gmail.com>
2024-09-17 02:41:47 +0530

    shirt schema added

---
+++ model/Tops.ts
@@ -0,0 +1,124 @@
+import mongoose from "mongoose";
+
+const topSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  strikePrice: {
+    type: Number,
+  },
+  fabric: {
+    cotton: Boolean,
+    linen: Boolean,
+    polyester: Boolean,
+    wool: Boolean,
+    silk: Boolean,
+    denim: Boolean,
+    velvet: Boolean,
+    fleece: Boolean,
+    nylon: Boolean,
+    hemp: Boolean,
+  },
+  design: {
+    solidColor: Boolean,
+    striped: Boolean,
+    checked: Boolean,
+    printed: Boolean,
+    graphic: Boolean,
+    embroidered: Boolean,
+    tieDye: Boolean,
+    colorBlocked: Boolean,
+    camouflage: Boolean,
+    floralPatterned: Boolean,
+  },
+  style: {
+    tShirt: Boolean,
+    basicTShirt: Boolean,
+    poloTShirt: Boolean,
+    henleyTShirt: Boolean,
+    vNeckTShirt: Boolean,
+    shirt: Boolean,
+    casualShirt: Boolean,
+    formalShirt: Boolean,
+    flannelShirt: Boolean,
+    denimShirt: Boolean,
+    linenShirt: Boolean,
+    buttonDownShirt: Boolean,
+    sweatshirt: Boolean,
+    hoodie: Boolean,
+    jacket: Boolean,
+    bomberJacket: Boolean,
+    denimJacket: Boolean,
+    leatherJacket: Boolean,
+    windbreaker: Boolean,
+    sweater: Boolean,
+    tankTop: Boolean,
+    longSleeveTop: Boolean,
+    cardigan: Boolean,
+  },
+  occasion: {
+    casual: Boolean,
+    formal: Boolean,
+    partyWear: Boolean,
+    sportswear: Boolean,
+    loungewear: Boolean,
+    workwear: Boolean,
+    vacation: Boolean,
+  },
+  season: {
+    summer: Boolean,
+    winter: Boolean,
+    allSeason: Boolean,
+    monsoonFriendly: Boolean,
+  },
+  fit: {
+    regularFit: Boolean,
+    slimFit: Boolean,
+    relaxedFit: Boolean,
+    oversized: Boolean,
+  },
+  sleeveType: {
+    shortSleeve: Boolean,
+    longSleeve: Boolean,
+    sleeveless: Boolean,
+    halfSleeve: Boolean,
+    rollUpSleeve: Boolean,
+  },
+  color: String,
+  size: {
+    s: Boolean,
+    m: Boolean,
+    l: Boolean,
+    xl: Boolean,
+    xxl: Boolean,
+  },
+  quantity: Number,
+  description: String,
+  mainImage: {
+    public_id: String,
+    secure_url: String,
+  },
+  additionalImage: [
+    {
+      public_id: String,
+      secure_url: String,
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Tops = mongoose.model("Tops", topSchema);
+
+export default Tops;
